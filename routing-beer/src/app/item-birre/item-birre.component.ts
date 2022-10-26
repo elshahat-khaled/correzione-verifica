bde14fffc4727b7798717cee32bea110c2666767
elshahat-khaled <elshahat.khaled@iisgalvanimi.edu.it>
2022-10-26 08:34:27 +0000

    punto 5 finito

--- routing-beer/src/app/item-birre/item-birre.component.ts
+++ routing-beer/src/app/item-birre/item-birre.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { BirreService } from '../birre.service';
-
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-item-birre',
   templateUrl: './item-birre.component.html',
@@ -15,21 +15,27 @@ export class ItemBirreComponent implements OnInit {
   items: any;
   birreServiceObs: Observable<Object> | undefined;
   constructor(
-  private route: ActivatedRoute,
-  private router: Router,
-  private service: BirreService
-  ) {}
+    private route: ActivatedRoute,
+    private router: Router,
+
+    private service: BirreService,
+    private location: Location,
+  ) { }
   ngOnInit(): void {
-  this.routeObs = this.route.paramMap;
-  this.routeObs.subscribe(this.getRouterParam);
+    this.routeObs = this.route.paramMap;
+    this.routeObs.subscribe(this.getRouterParam);
   }
   getRouterParam = (params: ParamMap) => {
-  console.log(params);
-  let itemId = params.get('id'); //Ottengo l'id dai parametri
-  console.log(itemId); //Stampo su console
-  this.birreServiceObs = this.service.searchDet(itemId);
-  this.birreServiceObs.subscribe(
-  (data) => ((this.items = data), console.log(data))
-  );
+    console.log(params);
+    let itemId = params.get('id'); //Ottengo l'id dai parametri
+    console.log(itemId); //Stampo su console
+    this.birreServiceObs = this.service.searchDet(itemId);
+    this.birreServiceObs.subscribe(
+      (data) => ((this.items = data), console.log(data))
+    );
   };
+  back(): void {
+    this.location.back();
   }
+}
+
